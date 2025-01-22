b9056eeb82f7918570b175e9d7f6ef558e250501
CaioSoares-V8 <caio.soares@v8.tech>
2025-01-22 11:14:58 -0300

    Add Firsts endpoints

--- application/IA-MVP/main.py
+++ application/IA-MVP/main.py
@@ -1,13 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
 from app.groqAPP import GroqClient
+import requests
 
 app = Flask(__name__)
 
+# Função para enviar uma requisição para a API do JAVA e armazenar o JSON de dados
+API_JAVA = '' # Link da API
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/fetch_data", methods=["GET"])
+def fetch_data():
+    response = requests.get(API_JAVA)
+    data = response.json()
+    return jsonify({"status": "success", "data": data})
 
+# Endpoint para retornar um JSON com o nível de aderência
+@app.route("/nivel_aderencia", methods=["GET"])
+def nivel_aderencia():
+    nivel_aderencia = {
+        "nivel": 0 
+    }
+    return jsonify(nivel_aderencia)
 
 if __name__ == '__main__':
     app.run()
@@ -16,9 +28,6 @@ class DataApplication:
     def __init__(self):
         self.groq_client = GroqClient()
 
-
-
-
 def main():
         question = "vai ser a response dos dados da vaga"
         print(f'{question}')
@@ -26,4 +35,6 @@ def main():
         print("IA Analisando os dados")
 
         response = self.groq_client.ask_question(question)
+
+
 
