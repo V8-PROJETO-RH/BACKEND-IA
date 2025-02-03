b905cf2ef8a4aa9d2c77f56a26df58dae7bcfcf4
pedrohpiress <pedro.psouza@sptech.school>
2025-02-03 14:10:58 -0300

    Resolvendo dependências

--- api-backend-java/login/src/main/java/br/com/v8/login/config/Config.java
+++ api-backend-java/login/src/main/java/br/com/v8/login/config/Config.java
@@ -1,11 +1,11 @@
 package br.com.v8.login.config;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -24,7 +24,7 @@ public class Config implements WebMvcConfigurer {
         config.addAllowedMethod("*");
 
         source.registerCorsConfiguration("/**", config);
-        return new CorsFilter();
+        return new CorsFilter(source);
     }
 
     @Bean
