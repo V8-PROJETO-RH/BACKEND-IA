f400492aee532d7dcc7abe03a40a5a283d9c0d5f
FilipeGuerreiro <filipe.guerreiro@sptech.school>
2025-02-13 10:45:02 -0300

    Adicionando configurações de ambiente e melhorias no projeto.
    Configuradas variáveis de ambiente para maior flexibilidade em múltiplos contextos, incluindo docker e perfis de teste. Incorporadas alterações nas dependências de teste e perfis no módulo login. Adicionados arquivos de configuração relevantes do IntelliJ IDEA para padronização do desenvolvimento.

--- api-backend-java/login/src/test/java/br/com/v8/login/LoginApplicationTests.java
+++ api-backend-java/login/src/test/java/br/com/v8/login/LoginApplicationTests.java
@@ -3,11 +3,12 @@ package br.com.v8.login;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.profiles.active=test"})
 class LoginApplicationTests {
 
 	@Test
 	void contextLoads() {
+		//TESTES
 	}
 
 }
