4a4281976c6b924a394edea5c0d55b29048906dd
FilipeGuerreiro <filipe.guerreiro@sptech.school>
2025-02-18 10:27:58 -0300

    Finalizando configuração de CORS e arrumando dependências.

--- api-backend-java/gateway/src/main/java/br/com/v8tech/gateway/infra/config/GatewayCorsConfig.java
+++ api-backend-java/gateway/src/main/java/br/com/v8tech/gateway/infra/config/GatewayCorsConfig.java
@@ -1,33 +1,16 @@
+
 package br.com.v8tech.gateway.infra.config;
 
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 public class GatewayCorsConfig implements WebMvcConfigurer {
-
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedOrigin("*"); // Permite todas as origens
-        corsConfig.addAllowedMethod("*"); // Permite todos os métodos (GET, POST, etc.)
-        corsConfig.addAllowedHeader("*"); // Permite todos os headers
-        corsConfig.setAllowCredentials(true);
-        corsConfig.setMaxAge(3600L); // Cache por 1 hora
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        return new CorsWebFilter(source);
-    }
 
     @Bean
     public RestTemplate restTemplate() {
